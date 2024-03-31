d65084cd807d78c3549819b065f90c5b1cadb545
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-03-31 10:59:05 +0200

    change db to postgress:

--- config/settings.py
+++ config/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default':  
       dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    if os.environ.get("PROD") else 
-   {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-   }     
+#     if os.environ.get("PROD") else 
+#    {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#    }     
 }
 
 # Password validation
