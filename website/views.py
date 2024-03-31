0054d83d4d650cb40533b13617568afed14149c5
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-03-31 11:06:13 +0200

    fix: login for staff

--- website/views.py
+++ website/views.py
@@ -14,10 +14,13 @@ import qrcode
 import os
 from django.contrib import messages
 from django.urls import reverse
+import json
+
 def login_view(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        data = json.loads(request.body)
+        email = data['email']
+        password = data['password']
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
